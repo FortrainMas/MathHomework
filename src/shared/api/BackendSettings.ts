2d80142a657ff572a88ff0c6e2a35ef9ebe19281
Ivan Shebanov <i.shebanov@g.nsu.ru>
2024-10-27 22:41:37 +0700

    Change backendSetting path

--- src/shared/api/BackendSettings.ts
+++ src/shared/api/BackendSettings.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default function initAxios() {
-    axios.defaults.baseURL = 'http://95.163.182.127:8080';
+    axios.defaults.baseURL = 'http://83.166.235.246:8080';
+//    axios.defaults.baseURL = 'http://95.163.182.127:8080';
     // это в ms таймаут, запомни и не ошибайся....
     axios.defaults.timeout = 600000;
 }
